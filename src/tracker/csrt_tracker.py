78bcf305af7bf14317757ac63806a25ed897ad23
Tapasya Gutta <tapasya234@gmail.com>
2026-02-16 01:03:52 +0530

    Implement multiple trackers.
    MOSSE and CSRT

--- src/tracker/csrt_tracker.py
+++ src/tracker/csrt_tracker.py
@@ -2,12 +2,12 @@ import cv2
 from src.tracker.base_tracker import Base_Tracker
 
 
-class MOSSE_Tracker(Base_Tracker):
+class CSRT_Tracker(Base_Tracker):
     def __init__(self):
         self.tracker = None
 
     def init(self, frame, boundary):
-        self.tracker = cv2.legacy.TrackerMOSSE().create()
+        self.tracker = cv2.legacy.TrackerCSRT().create()
         self.tracker.init(frame, tuple(boundary))
 
     def update(self, frame):
--- src/tracker/tracker_creator.py
+++ src/tracker/tracker_creator.py
@@ -1,9 +1,10 @@
 from logger import logger
 
-import tracker_constants
+import src.tracker.tracker_constants as tracker_constants
 
-from mosse_tracker import MOSSE_Tracker
-from base_tracker import Base_Tracker
+from src.tracker.mosse_tracker import MOSSE_Tracker
+from src.tracker.csrt_tracker import CSRT_Tracker
+from src.tracker.base_tracker import Base_Tracker
 
 # from trackers.kalman_tracker import KalmanTracker
 
@@ -12,6 +13,10 @@ def create_tracker(tracker_type: str):
     if tracker_type == tracker_constants.TRACKER_MOSSE:
         logger.info("Using tracker type %s", tracker_type)
         return MOSSE_Tracker()
+
+    if tracker_type == tracker_constants.TRACKER_CSRT:
+        logger.info("Using tracker type %s", tracker_type)
+        return CSRT_Tracker()
 
     logger.warning("Unknown tracker type %s, defaulting to Base_Tracker", tracker_type)
     return Base_Tracker()
